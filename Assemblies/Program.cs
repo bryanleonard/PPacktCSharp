72169f2f48ade0e0c5317d7554dd8d4524401dfa
Bryan Leonard <bleonard@acrtinc.com>
2018-03-15 16:58:54 -0400

    Add Shared Library

---
+++ Assemblies/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var doc = new XDocument();
+        }
+    }
+}
