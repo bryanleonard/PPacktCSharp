4bcd6553059eaeb138e30ddc18cd679a054fcc02
Bryan Leonard <bleonard@acrtinc.com>
2018-03-19 14:10:12 -0400

    Commit message.

--- WorkingWithLists/Program.cs
+++ WorkingWithLists/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using static System.Console;
+using System.Collections.Immutable;
 
 namespace WorkingWithLists
 {
@@ -8,8 +9,51 @@ namespace WorkingWithLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //https://www.packtpub.com/mapt/book/all_books/9781788398077/10/ch10lvl1sec69/working-with-collections
+            var cities = new List<string> { "London", "Paris", "Milan", "Akron", "New York" };
+            //instead of 
+            //var cities = new List<string>();
+            //cities.Add("London");
+
+            WriteLine("Initial list:");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+
+            WriteLine($"The first city is {cities[0]}.");
+            WriteLine($"The last city is {cities[cities.Count - 1]}.");
+            cities.Insert(0, "Sydney");
+            WriteLine("After inserting Sydney at index 0");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+
+            cities.RemoveAt(1);
+            cities.Remove("Milan");
+            WriteLine("After removing two cities");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+
+            var immutableCities = cities.ToImmutableList();
+
+            var newList = immutableCities.Add("Rio"); //does't work bc immutable :)
+
+            WriteLine("\nImmutable cities:");
+            foreach (string city in immutableCities)
+            {
+                WriteLine($" {city}");
+            }
+            WriteLine();
+
+            WriteLine("New cities:");
+            foreach (string city in newList)
+            {
+                WriteLine($" {city}");
+            }
+            WriteLine();
         }
     }
 }
